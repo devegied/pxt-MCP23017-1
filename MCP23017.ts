6ff8f309d3dd8e9b90fb7674d2b52bed331ac902
devegied <24810903+devegied@users.noreply.github.com>
2019-08-02 16:15:19 +0300

    move enum and globals inside namespace

--- MCP23017.ts
+++ MCP23017.ts
@@ -1,48 +1,46 @@
-
 /**
  *  MCP23017-control blocks
  */
-
-let outputABuffer = 0;
-let outputBBuffer = 0;
-
-enum SET_PORT {
-    //% block=PORT_A
-    A = 0,
-    //% block=PORT_B
-    B = 256
-}
-
-enum REG_PIO {
-    //% block=PORT_A
-    A = 4608,
-    //% block=PORT_B
-    B = 4864
-}
-enum ADDRESS {                     // address for MCP23017 (configurable by tying pins 15,16,17 on the mcp23017 high or low)
-    //% block=0x20
-    A20 = 0x20,               // 
-    //% block=0x21
-    A21 = 0x21,                // 
-    //% block=0x22
-    A22 = 0x22,                // 
-    //% block=0x23
-    A23 = 0x23,                // 
-    //% block=0x24
-    A24 = 0x24,                // 
-    //% block=0x25
-    A25 = 0x25,                // 
-    //% block=0x26
-    A26 = 0x26,                // 
-    //% block=0x27
-    A27 = 0x27                // 
-}
-
-/**
- * Blocks
- */
-"//% weight=100 color=#0fbc12 icon="
+//% weight=100 color=#0fbc12 icon="\uf192" block="MCP23017"
 namespace MCP23017 {
+
+    let outputABuffer = 0;
+    let outputBBuffer = 0;
+
+    export enum SET_PORT {
+        //% block=PORT_A
+        A = 0,
+        //% block=PORT_B
+        B = 256
+    }
+
+    export enum REG_PIO {
+        //% block=PORT_A
+        A = 4608,
+        //% block=PORT_B
+        B = 4864
+    }
+
+    // address for MCP23017 (configurable by tying pins 15,16,17 on the mcp23017 high or low)
+    export enum ADDRESS {
+        //% block=0x20
+        A20 = 0x20,               // 
+        //% block=0x21
+        A21 = 0x21,                // 
+        //% block=0x22
+        A22 = 0x22,                // 
+        //% block=0x23
+        A23 = 0x23,                // 
+        //% block=0x24
+        A24 = 0x24,                // 
+        //% block=0x25
+        A25 = 0x25,                // 
+        //% block=0x26
+        A26 = 0x26,                // 
+        //% block=0x27
+        A27 = 0x27                // 
+    }
+
     //% block
     export function clearAllOuputsOn(adress: ADDRESS, port: REG_PIO) {
         pins.i2cWriteNumber(adress, port + 0, NumberFormat.UInt16BE)
@@ -52,8 +50,6 @@ namespace MCP23017 {
     export function setAllOuputsOn(adress: ADDRESS, port: REG_PIO) {
         pins.i2cWriteNumber(adress, port + 0B11101111, NumberFormat.UInt16BE)
     }
-
-
 
     //% block
     export function setOutputA(bit: number) {
@@ -129,5 +125,4 @@ namespace MCP23017 {
     export function ReadNotAnd(addr: ADDRESS, reg: REG_PIO, value: number): boolean {
         return (!(readRegister(addr, reg) & value))
     }
-
 }
